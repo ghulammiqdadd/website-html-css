dc5256eb9d761dbce4faa376a801b5a66fad1747
ghulammiqdadd <ghulammiqdad35@gmail.com>
2026-03-13 00:02:38 +0700

    update css html js

--- scrip.js
+++ scrip.js
@@ -14,3 +14,66 @@ document.addEventListener('click', function (e) {
     }
 
 });
+
+//let cart = 0;
+
+//document.querySelectorAll(".btn-cart").forEach(button => {
+   // button.addEventListener("click", () => {
+   //     cart++;
+    //    document.getElementById("cart-count").textContent = cart;
+   // });
+//});
+
+let cart = [];
+
+const cartItems = document.getElementById("cart-items");
+const cartCount = document.getElementById("cart-count");
+const cartTotal = document.getElementById("cart-total");
+
+document.querySelectorAll(".btn-cart").forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const name = button.dataset.name;
+        const price = parseInt(button.dataset.price);
+
+        cart.push({name, price});
+
+        updateCart();
+
+    });
+
+});
+
+function updateCart(){
+
+    cartItems.innerHTML = "";
+
+    let total = 0;
+
+    cart.forEach((item,index)=>{
+
+        total += item.price;
+
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+        ${item.name} - Rp ${item.price}
+        <button onclick="removeItem(${index})">Hapus</button>
+        `;
+
+        cartItems.appendChild(li);
+
+    });
+
+    cartCount.textContent = cart.length;
+    cartTotal.textContent = total.toLocaleString("id-ID");
+
+}
+
+function removeItem(index){
+
+    cart.splice(index,1);
+    updateCart();
+
+}
